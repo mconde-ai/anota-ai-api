8cca37b31d43f53109b4083906eacbefc7b7a7f6
Marcondes Fernandes de Oliveira Filho <marcondes.filho@anota.ai>
2025-10-02 21:54:02 -0300

    fix: implement correct serverless handler for Vercel

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { HttpAdapterHost } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+// Função para configurar a aplicação.
+async function configureApp(app: INestApplication) {
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   app.useGlobalPipes(new ValidationPipe({
@@ -26,10 +26,30 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
-
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  return expressApp;
 }
 
-export default bootstrap();
+// Lógica para rodar localmente (npm run start:dev)
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await configureApp(app); // Aplica as configurações
+  await app.listen(3000);
+}
+
+// Se não estiver no ambiente Vercel, roda o servidor local
+if (!process.env.VERCEL) {
+  bootstrap();
+}
+
+// Lógica para o Vercel (o que será exportado)
+let cachedApp: INestApplication;
+
+export default async function handler(req, res) {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule);
+    await configureApp(app); // Aplica as configurações
+    await app.init();
+    cachedApp = app;
+  }
+  const expressApp = cachedApp.getHttpAdapter().getInstance();
+  expressApp(req, res);
+}
